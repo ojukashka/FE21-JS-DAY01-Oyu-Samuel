38764fbd5bb61f7f21fd7b38ded9caac16df99d2
Samuel Tenschert <samuel.tenschert@gmail.com>
2021-03-03 12:59:07 +0100

    intermediate

--- basic_samuel/scriptbasic.js
+++
@@ -1,3 +0,0 @@
-console.log ("Hello my name is Samuel and I am " + x + " years old")
-players=["Martin", "Thomas", "Peter", "Matthias", "Niki"];
-console.log ("The most valuable player of the match is " + players[2])
